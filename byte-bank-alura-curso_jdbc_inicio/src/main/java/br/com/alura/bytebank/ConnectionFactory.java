ef7ba9842466fcbea1dbf9c59ca78430ce248e8c
Gustavo Henrique Alves Maciel <gustavoh.a.m1409@gmail.com>
2025-11-09 20:49:00 -0300

    adicionando uma pool de conexões

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -2,17 +2,31 @@ package br.com.alura.bytebank;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 
 public class ConnectionFactory {
 
     public Connection recuperarConexao(){
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
-            return connection;
+            
+            return createDataSource().getConnection();
             
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("root");
+        config.setMaximumPoolSize(10);
+
+        return new HikariDataSource(config);
     }
 }
